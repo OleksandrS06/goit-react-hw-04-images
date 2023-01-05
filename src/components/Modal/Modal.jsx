1200b98e0f6bca1671c61116bc5c9f3b0e47bd01
OleksandrS06 <112249299+OleksandrS06@users.noreply.github.com>
2023-01-05 22:50:04 +0100

    modal useEffect update

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,21 +4,29 @@ import { createPortal } from 'react-dom';
 const modalRootRef = document.getElementById('modal-root');
 
 const Modal = ({ url, alt, toggleModal }) => {
-  useEffect(() => {
-    document.addEventListener('keydown', handleKeyDown);
-  }, []);
+  // useEffect(() => {
+  //   document.addEventListener('keydown', handleKeyDown);
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
 
   useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        toggleModal();
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
     return () => {
       document.removeEventListener('keydown', handleKeyDown);
     };
   }, []);
 
-  const handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      toggleModal();
-    }
-  };
+  // const handleKeyDown = e => {
+  //   if (e.code === 'Escape') {
+  //     toggleModal();
+  //   }
+  // };
 
   const handleClick = e => {
     if (e.target === e.currentTarget) {
